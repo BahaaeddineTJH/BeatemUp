2f6c0330fdb75295cc06a26e80a993fddb462236
Bahaaeddine TAHRI JOUTEY HASSANI <bahaaeddine.tahri-joutey-hassani@epita.fr>
2020-12-02 21:39:14 +0100

    yes

---
+++ ProjectMouvement/Assets/thirdmouvement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class thirdmouvement : MonoBehaviour
+{
+    public CharacterController controller;
+
+    public float speed = 6f;
+
+    public float turnSmoothTime = 0.1f;
+    private float turnsmoorthvelocity;
+    public Transform cam;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        
+        Vector3 direction = new Vector3(horizontal,0f,vertical).normalized;
+
+        if (direction.magnitude >= 0.1f)
+        {
+            float targetangle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetangle, ref turnsmoorthvelocity,turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+            Vector3 movedirection = Quaternion.Euler(0f, angle, 0f) * Vector3.forward;
+            controller.Move(movedirection.normalized * speed * Time.deltaTime);
+        }
+    }
+}
